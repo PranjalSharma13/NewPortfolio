63eaec936b3da9dbd0e1e85f4bfa5a7f0eed0942
PranjalSharma13 <pranjal.sharm98@gmail.com>
2025-10-22 19:11:52 -0400

    imported package

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import './index.css'
 import App from './App.tsx'
 import { AppThemeProvider } from './provider/ThemeProvider.tsx'
 import './i18n/i18n.ts'
+import { ParallaxProvider } from 'react-scroll-parallax'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppThemeProvider>
-    <App />
-    </AppThemeProvider>
+    <ParallaxProvider>
+      <AppThemeProvider>
+        <App />
+      </AppThemeProvider>
+    </ParallaxProvider>
   </StrictMode>,
 )
